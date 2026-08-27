60e215455a80caf26fde56111a91e1b9d26841e4
Guilherme <guimars22@gmail.com>
2026-08-27 16:49:25 -0300

    #

---
+++ agents/analista.py
@@ -0,0 +1,19 @@
+
+from dotenv import load_dotenv
+
+from agno.agent import Agent
+from agno.models.groq import Groq
+from agno.tools.yfinance import YFinanceTools
+
+load_dotenv()
+
+agent = Agent(
+    model=Groq(id="openai/gpt-oss-20b"),
+    tools=[YFinanceTools()],
+    markdown=True,
+    instructions="Use tabelas para motrar informações final. Não inclua nenhum outro texto"
+)
+
+agent.print_response(
+    "Qual é a contação atual da APPLE"
+)
