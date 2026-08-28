190d2a5d420809aca7a3b5b2f4638a5773c51a3a
Guilherme <guimars22@gmail.com>
2026-08-28 17:26:28 -0300

    #

--- agents/analista.py
+++ agents/analista.py
@@ -1,19 +1,27 @@
-
-from dotenv import load_dotenv
-
 from agno.agent import Agent
+from agno.db.sqlite import SqliteDb
 from agno.models.groq import Groq
 from agno.tools.yfinance import YFinanceTools
+
+import os
+from dotenv import load_dotenv
+
+db = SqliteDb(db_file="tmp/agno.db")
 
 load_dotenv()
 
 agent = Agent(
+    name="Analista Financeiro",
+    session_id='session_1',
+    user_id='user_id',
     model=Groq(id="openai/gpt-oss-20b"),
     tools=[YFinanceTools()],
-    markdown=True,
-    instructions="Use tabelas para motrar informações final. Não inclua nenhum outro texto"
+    instructions="Use tabelas para motrar informações final. Não inclua nenhum outro texto",
+    db=db,
+    add_history_to_context=True,
+    num_history_runs=3,
 )
 
 agent.print_response("Qual é a contação Petrobras")
 agent.print_response("Qual é a contação Vale")
-agent.print_response("Quausi empresas já consultamos a cotação")
+agent.print_response("Quais empresas já consultamos a cotação")
